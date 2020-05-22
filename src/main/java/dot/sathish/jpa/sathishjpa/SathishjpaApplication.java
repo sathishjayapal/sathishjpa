e0cd2a2dce6650910f635b6d17c3d54e3ef475ab
Sathish Jayapal <sjayapal@skminfotech.com>
2020-05-22 08:34:22 -0500

    Initial commit

---
+++ src/main/java/dot/sathish/jpa/sathishjpa/SathishjpaApplication.java
@@ -0,0 +1,13 @@
+package dot.sathish.jpa.sathishjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SathishjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SathishjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dot/sathish/jpa/sathishjpa/SathishjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package dot.sathish.jpa.sathishjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SathishjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
